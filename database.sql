92166b8c9773d5f5420ced7c5cc8996f6e065d88
Richard Wilsher <57815952+RichardWilsher@users.noreply.github.com>
2024-06-16 17:10:52 +0100

    SQL database structure change

--- database.sql
+++ database.sql
@@ -7,11 +7,13 @@ ability varchar(20),
 name varchar(15)
 );
 ALTER TABLE cpr.roles CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.skill_category (
 id int,
 name varchar(45)
 );
 ALTER TABLE cpr.skill_category CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.skills (
 id int,
 name varchar(45),
@@ -23,23 +25,24 @@ description varchar(4000),
 example varchar(4000)
 );
 ALTER TABLE cpr.skills CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.stats (
 id int,
 name varchar(45),
 modified varchar(1)
 );
 ALTER TABLE cpr.stats CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mooks (
 id int,
 name varchar(45),
 mook_type int,
 type int,
-headsp int,
-bodysp int,
 location varchar(45),
 rep int
 );
 ALTER TABLE cpr.mooks CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_weapon (
 id int,
 mookid int,
@@ -47,6 +50,7 @@ weaponid int,
 qualityid int
 );
 ALTER TABLE cpr.mook_weapon CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_stat (
 id int,
 mookid int,
@@ -54,6 +58,7 @@ statid int,
 value varchar(8)
 );
 ALTER TABLE cpr.mook_stat CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_skill (
 id int,
 mookid int,
@@ -62,6 +67,7 @@ value varchar(8),
 notes varchar(20)
 );
 ALTER TABLE cpr.mook_skill CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_role (
 id int,
 mookid int,
@@ -70,6 +76,7 @@ value varchar(2),
 notes varchar(45)
 );
 ALTER TABLE cpr.mook_role CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_equipment (
 id int,
 mookid int,
@@ -78,6 +85,7 @@ quantity varchar(8),
 subinformation varchar(60)
 );
 ALTER TABLE cpr.mook_equipment CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_cyberwear (
 id int,
 mookid int,
@@ -86,33 +94,39 @@ quantity varchar(8),
 notes varchar(80)
 );
 ALTER TABLE cpr.mook_cyberwear CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.type (
 id int,
 name varchar(45)
 );
 ALTER TABLE cpr.type CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.equipment_category (
 id int,
 name varchar(45)
 );
 ALTER TABLE cpr.equipment_category CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.equipment (
 id int,
 categoryid int,
 name varchar(45)
 );
 ALTER TABLE cpr.equipment CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.cyberwear_category (
 id int,
 name varchar(45)
 );
 ALTER TABLE cpr.cyberwear_category CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.cyberwear (
 id int,
 categoryid int,
 name varchar(45)
 );
 ALTER TABLE cpr.cyberwear CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.armour (
 id int,
 name varchar(45),
@@ -120,6 +134,7 @@ sp varchar(2),
 modifier varchar(2)
 );
 ALTER TABLE cpr.armour CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.weapons (
 id int,
 name varchar(45),
@@ -127,13 +142,31 @@ dice varchar(4),
 rof varchar(1)
 );
 ALTER TABLE cpr.weapons CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.weapon_quality (
 id int,
 name varchar(10)
 );
 ALTER TABLE cpr.weapon_quality CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
 CREATE TABLE cpr.mook_type (
 id int,
 name varchar(45)
 );
 ALTER TABLE cpr.mook_type CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
+CREATE TABLE cpr.mook_body_armour (
+id int,
+mookid int,
+armourid int,
+techupgraded varchar(1)
+);
+ALTER TABLE cpr.mook_body_armour CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
+
+CREATE TABLE cpr.mook_head_armour (
+id int,
+mookid int,
+armourid int,
+techupgraded varchar(1)
+);
+ALTER TABLE cpr.mook_head_armour CHANGE COLUMN id id INT NOT NULL AUTO_INCREMENT , ADD PRIMARY KEY (id);
